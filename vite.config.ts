b91bf5d920ad752a4771e84f67eaffe7ad30f0e4
bq-t <w.bequiet@gmail.com>
2025-03-13 23:37:48 +0400

    chore: update github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/giphy-clips/' : '/',
   plugins: [
     vue(),
     svgLoader(),
